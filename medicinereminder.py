6e5c33c44cfebd2be66b4f3c44b90f491eff9284
Josna Joby <josnajoby417@gmail.com>
2026-03-29 23:46:24 +0530

    Update medicinereminder.py

--- medicinereminder.py
+++ medicinereminder.py
@@ -1,1 +1,14 @@
-
+import time
+medicines=[]
+n=int(input("How many medicines do you want to add?"))
+for i in range(n):
+  name=input("Enter medicine name: ")
+  medtime=input("Enter time(HH:MM in 24hr format):")
+  medicines.append((name,medtime))
+print("\nReminder started...")
+while True:
+  currentime=time.strftime("%H:%M")
+  for med in medicines:
+    if med[1]==currentime:
+        print(f"\nTime to take your medicine:{med[0]}")
+  time.sleep(60)
